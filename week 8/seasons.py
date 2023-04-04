255fb411d66aabfea059070186030ebefea33ff8
KariHab <habboutk@outlook.com>
2023-04-04 13:22:40 -0400

    valid

--- week 8/seasons.py
+++ week 8/seasons.py
@@ -8,14 +8,13 @@ p = inflect.engine()
 def main():
     print(get_birthday(input("Date of birth: ")))
 
-
 def get_birthday(s):
     if matches := re.search(r"^([1-2]\d{3})-(0\d|1[0-2])-(0\d|1\d|2\d|30?1?)$", s):
         age = date.today() - date(int(matches.group(1)), int(matches.group(2)), int(matches.group(3)))
         return f'{p.number_to_words(age.days * 24 * 60, andword= "").capitalize()} minutes'
-
     else:
         sys.exit("invalid date")
+
 
 if __name__ == "__main__":
     main()
